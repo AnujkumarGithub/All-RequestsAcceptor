23fa07eb943d64b4c228f5cd656299079008f7bb
Akvcontact <anujkumarverma176@Gmail.com>
2024-06-02 14:24:37 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -6,7 +6,7 @@ from pyrogram.errors import FloodWait
 
 logging.basicConfig(level=logging.ERROR)
        
-SESSION = environ.get("SESSION", "")        
+SESSION = environ.get("SESSION", "BQC6kfsAe8NFfnuNvAVx2e9I02qXZ9gYKi1MwgKr4wUgpcLsiUznlE3A6Ppdyd-qSCAW3DRUus1qlNZpD3-rLXG7In6oQXaX3vGpR0_SB7O7v6dx0lxeZ5gChkUCEqT7cF1CCpXF2cGYVBiMXhTPNRcoBJs4DGeMKgEO3A2HkcAnmzQ5A3gRNGufi_lPqGRK5WXHBKpS8cqu00sFOT2rR8w1iRxXVuY-B28_MFGTE_eOe51xLurEB25O2jUcZSJSOFUrW3aamX_IsR9Tc8sMiaAQ-WmiyUf0nlWAActljfxnSIgiYyABAXNeVOOUN762tA6PAp6IgP7ZY8QV3g9O77IOXNSooQAAAAB_2impAA")        
 User = Client(name="AcceptUser", session_string=SESSION)
 
 
